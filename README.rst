cf77385929302987b63723484d3f78968f404e0d
Huang Rui <bjhuangr@cn.ibm.com>
2014-11-03 15:20:08 +0800

    The init commmit of stackforge/zvm-driver
    Including all python modules for nova-zvm-virt-driver and
    neutron-zvm-plugin.
    
    Change-Id: I72dd9f64fc412cbf10f5e7ab6e4ac465a977e849

---
+++ README.rst
@@ -0,0 +1,5 @@
+This project contains a set of drivers and plugins for different OpenStack 
+components that enables OpenStack manage z/VM hypervisor and virtual machines
+running in the z/VM system.
+
+Wiki page: https://wiki.openstack.org/wiki/ZVMDriver
---
+++ nova-zvm-virt-driver/nova/virt/zvm/vif.py
@@ -0,0 +1,45 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import abc
+
+from nova.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ZVMBaseVIFDriver(object):
+    @abc.abstractmethod
+    def plug(self, instance, vif):
+        pass
+
+    @abc.abstractmethod
+    def unplug(self, instance, vif):
+        pass
+
+
+class ZVMNeutronVIFDriver(ZVMBaseVIFDriver):
+    """Neutron VIF driver."""
+
+    def plug(self, instance, vif):
+        # Neutron takes care of plugging the port
+        pass
+
+    def unplug(self, instance, vif):
+        # Neutron takes care of unplugging the port
+        pass
